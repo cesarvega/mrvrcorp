9d5823c37e659d03823395528a507cbd5064e2fe
cesarvega <cesarvega.col@gmail.com>
2019-08-27 17:48:51 -0400

    excell reader ready, I comment thew file upload to only get execell files

--- src/app/main/dynamic-profile/components/file-uploader/file-uploader.component.ts
+++ src/app/main/dynamic-profile/components/file-uploader/file-uploader.component.ts
@@ -4,7 +4,8 @@ import { FormGroup } from '@angular/forms';
 import { FieldConfig2 } from '../../field.interface';
 import { ToastrService } from 'ngx-toastr';
 import { PDFDocumentProxy } from 'pdfjs-dist';
-
+import * as XLSX from 'xlsx';
+const { read, write, utils } = XLSX;
 import { ImageResult, ResizeOptions } from 'ng2-imageupload';
 import { FileUploadService } from './file-upload-service.service';
 @Component({
@@ -34,10 +35,13 @@ export class FileUploaderComponent implements OnInit {
   fileToUpload: File = null;
   pictures: Array<any> = [];
   maxAmountofFiles = 4;
-  @ViewChild('video', {static: false})
+  file: File;
+  
+  arrayBuffer: any;
+  @ViewChild('video', { static: false })
   public video: ElementRef;
 
-  @ViewChild('canvas', {static: false})
+  @ViewChild('canvas', { static: false })
   public canvas: ElementRef;
 
   public captures: Array<any>;
@@ -60,29 +64,68 @@ export class FileUploaderComponent implements OnInit {
     });
   }
 
-  selected(imageResult: ImageResult): void {
-    if (this.imageCounter > this.maxAmountofFiles || this.pictures.length >= this.maxAmountofFiles - 1) {
-      this.toastr.warning('Please select max 3 files.');
-    } else {
-      this.src = imageResult.resized
-        && imageResult.resized.dataURL
-        || imageResult.dataURL;
-      if (imageResult.file.size > 2097152) {
-        this.toastr.warning('File is too big! maximun size is 2Mb');
-      } else {
-        this._FileUploadService.postInfo(imageResult.file).subscribe(x => {
-          const docId = x[0].docId;
-          if (imageResult.file.type === 'application/pdf') {
-            this.pictureData = { data: imageResult.dataURL, name: imageResult.file.name, format: imageResult.file.type, docId: docId };
-          } else {
-            this.pictureData = { data: imageResult.resized.dataURL, name: imageResult.file.name, format: imageResult.file.type, docId: docId };
-          }
-          this.imageCounter = this.imageCounter + 1;
-          this.pictures.push(this.pictureData);
-        });
-
+  incomingfile(event): void {
+    this.file = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      this.arrayBuffer = fileReader.result;
+      const data = new Uint8Array(this.arrayBuffer);
+      const arr = new Array();
+      for (let i = 0; i !== data.length; ++i) {
+        arr[i] = String.fromCharCode(data[i]);
       }
-    }
+      const bstr = arr.join('');
+      const workbook = XLSX.read(bstr, { type: 'binary' });
+      // tslint:disable-next-line:variable-name
+      const first_sheet_name = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[first_sheet_name];
+      console.log(XLSX.utils.sheet_to_json(worksheet, { raw: true }));
+    };
+    fileReader.readAsArrayBuffer(this.file);
+  }
+
+  selected(imageResult: any): void {
+    const bstr: string = imageResult.dataURL;
+    const wbb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+    const wsname: string = wbb.SheetNames[0];
+    const ws: XLSX.WorkSheet = wbb.Sheets[wsname];
+
+    const reader: FileReader = new FileReader();
+    // reader.readAsText(imageResult.dataURL);
+    reader.onload = (e) => {
+      const res = reader.result as string; // This variable contains your file as text
+      const lines = res.split('\n'); // Splits you file into lines
+      const ids = [];
+      lines.forEach((line) => {
+        ids.push(line.split(',')[0]); // Get first item of line
+      });
+      console.log(ids);
+    };
+
+    reader.readAsDataURL(imageResult.data);
+    // const wb: XLSX.WorkBook = XLSX.read(imageResult, {type: 'array'});
+    // if (this.imageCounter > this.maxAmountofFiles || this.pictures.length >= this.maxAmountofFiles - 1) {
+    //   this.toastr.warning('Please select max 3 files.');
+    // } else {
+    //   this.src = imageResult.resized
+    //     && imageResult.resized.dataURL
+    //     || imageResult.dataURL;
+    //   if (imageResult.file.size > 2097152) {
+    //     this.toastr.warning('File is too big! maximun size is 2Mb');
+    //   } else {
+    //     this._FileUploadService.postInfo(imageResult.file).subscribe(x => {
+    //       const docId = x[0].docId;
+    //       if (imageResult.file.type === 'application/pdf') {
+    //         this.pictureData = { data: imageResult.dataURL, name: imageResult.file.name, format: imageResult.file.type, docId: docId };
+    //       } else {
+    //         this.pictureData = { data: imageResult.resized.dataURL, name: imageResult.file.name, format: imageResult.file.type, docId: docId };
+    //       }
+    //       this.imageCounter = this.imageCounter + 1;
+    //       this.pictures.push(this.pictureData);
+    //     });
+
+    //   }
+    // }
   }
 
   deleteDocument(index, docId): any {
