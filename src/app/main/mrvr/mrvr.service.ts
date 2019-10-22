a2e7afb942730221c38621f126abd8f1e7bb6798
cesarvega <cesarvega.col@gmail.com>
2019-10-22 14:58:56 -0400

    commit new component

--- src/app/main/mrvr/mrvr.service.ts
+++ src/app/main/mrvr/mrvr.service.ts
@@ -2,18 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' , 'Access-Control-Allow-Origin': '*' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class MrvrService {
 
-  // baseUrl = 'https://us-central1-bsgur-fdb7a.cloudfunctions.net/app/';
-  baseUrl = 'http://localhost:5001/bsgur-fdb7a/us-central1/app';
+  baseUrl = 'https://us-central1-bsgur-fdb7a.cloudfunctions.net/app/';
+  // baseUrl = 'http://localhost:5001/bsgur-fdb7a/us-central1/app';
   constructor(private httpClient: HttpClient) { }
 
 
   getItemData(): Observable<any> {
-    return this.httpClient.get(this.baseUrl);
+    return this.httpClient.get(this.baseUrl, httpOptions);
   }
 }
