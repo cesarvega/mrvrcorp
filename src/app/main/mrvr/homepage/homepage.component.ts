e51017c62545227e6d4e8c397e96e67128a45680
cesarvega <cesarvega.col@gmail.com>
2019-11-07 16:21:48 -0500

    COMMIT FLEX LAYOUT BASE NAIL

--- src/app/main/mrvr/homepage/homepage.component.ts
+++ src/app/main/mrvr/homepage/homepage.component.ts
@@ -2,19 +2,26 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FuseConfigService } from '@fuse/services/config.service';
 import { MrvrService } from '../mrvr.service';
 import { fuseAnimations } from '@fuse/animations';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss'],
-  encapsulation: ViewEncapsulation.None,
-  animations: fuseAnimations
+  styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
 
+  reasons = ['Survey Technical Questions/Comments', 'Payment Questions',
+    'Website Questions/Comments', 'Profile/Registration Questions Comments', 'Change Username'];
+  stripeForm: FormGroup;
+  formErrors: any;
+  card: any;
+  error: string;
+  emailAddress: any;
   constructor(
     private _fuseConfigService: FuseConfigService,
-    private _mrvrservice: MrvrService
+    private _mrvrservice: MrvrService,
+    public fb: FormBuilder
   ) {
     // Configure the layout
     this._fuseConfigService.config = {
@@ -33,8 +40,49 @@ export class HomepageComponent implements OnInit {
         }
       }
     };
-  }
-  ngOnInit(): void {
+
+    this.formErrors = {
+      senderName: {},
+      emailFrom: {},
+      amount: {},
+      cardNumber: {},
+      expDate: {},
+      ccv: {},
+      zipcode: {},
+      billingAddress: {},
+      TypeOfInquiry: {},
+      SurveyName: {},
+    };
+
   }
 
+
+  ngOnInit(): void {
+    this.stripeForm = this.fb.group({
+      senderName: ['cesar', [Validators.required, Validators.minLength(4)]],
+      emailFrom: ['cvega@gmail.com', [Validators.required, Validators.email]],
+      amount: ['13', [Validators.required, Validators.minLength(5)]],
+      cardNumber: ['4242424242424242', [Validators.required, Validators.minLength(5)]],
+      expDate: ['07 23', [Validators.required, Validators.minLength(5)]],
+      ccv: ['143', [Validators.required, Validators.minLength(5)]],
+      zipcode: ['33131', [Validators.required, Validators.minLength(5)]],
+      billingAddress: ['201 SE 2 ave', [Validators.required, Validators.minLength(5)]],
+      TypeOfInquiry: ['', Validators.required],
+      SurveyName: [''],
+    });
+  }
+
+  async onSubmit(): Promise<void> {
+    console.log(this.stripeForm.value);
+    const { token, error } = await stripe.createToken(this.card, {
+      email: this.emailAddress
+    });
+
+    if (error) {
+      console.log('Something is wrong:', error);
+    } else {
+      console.log('Success!', token);
+      // ...send the token to the your backend to process the charge
+    }
+  }
 }
